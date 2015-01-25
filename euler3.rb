821ffa7021a921ff6384c5449ec39e2cbf418500
Kingdon <yebyen@gmail.com>
2015-01-25 14:44:00 -0500

    pass

--- euler3.rb
+++ euler3.rb
@@ -4,14 +4,14 @@ require 'prime'
 # What is the largest prime factor of the number 600851475143 ?
 
 class LPF
-def self.lpf(f)
-  fs=[]
-  Math.sqrt(f).to_i.downto(2) do |a|
-    if f % a == 0
-      fs<<a
-      return a if a.prime?
+  def self.lpf(fun)
+    Math.sqrt(fun).to_i.downto(2) do |a|
+      if fun % a == 0
+        #puts "fun % a == #{fun} % #{a} == #{fun % a}"
+        b = fun/a
+        return b if b.prime?
+        return a if a.prime?
+      end
     end
   end
-  return fs
-end
 end
