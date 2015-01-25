d26a3f76510b791834f4a8f3e74b94f8669d2a08
Kingdon <yebyen@gmail.com>
2015-01-25 17:22:05 -0500

    more passes

--- spec/lpf_spec.rb
+++ spec/lpf_spec.rb
@@ -4,7 +4,10 @@ describe LPF do
   it "largest_prime_factor 13195" do
     expect(LPF.lpf(13195)).to eql 29
   end
-  it "largest_prime_factor 6008514" do
+  it "largest_prime_factor 600851" do
+    expect(LPF.lpf(600851)).to eql 20719
+  end
+  it "largest_prime_factor 6008514 (7 digits)" do
     expect(LPF.lpf(6008514)).to eql 58907
   end
   it "largest_prime_factor 60085147 (8 digits)" do
@@ -13,11 +16,26 @@ describe LPF do
   it "largest_prime_factor 600851475 (9 digits)" do
     expect(LPF.lpf(600851475)).to eql 54499
   end
+  it "largest_prime_factor 6008514751 (10 digits)" do
+    expect(LPF.lpf 6008514751).to eql 3439333
+  end
   it "largest_prime_factor 60085147514 (11 digits)" do
     expect(LPF.lpf 60085147514).to eql 10976461
   end
   it "largest_prime_factor 600851475143 (12 digits)" do
     expect(LPF.lpf(600851475143)).to eql 6857
+  end
+  it "largest_prime_factor 6008514751432 (13 digits)" do
+    expect(LPF.lpf(6008514751432)).to eql 574647547
+  end
+  it "largest_prime_factor 60085147514328 (14 digits)" do
+    expect(LPF.lpf(60085147514328)).to eql 278171979233
+  end
+  it "largest_prime_factor 600851475143283 (15 digits)" do
+    expect(LPF.lpf(600851475143283)).to eql 3781567
+  end
+  it "largest_prime_factor 6008514751432834 (16 digits)" do
+    expect(LPF.lpf(6008514751432834)).to eql 16598107048157
   end
   it "lpf 10976461*2 (10976461)" do
     n = 10976461
@@ -25,5 +43,3 @@ describe LPF do
     expect(LPF.lpf t).to eql 10976461
   end
 end
-#it "should be faster"
-#it "should be faster"
