13859b172cdd51bdeafd580f69a9df4ab35b4391
Kingdon <yebyen@gmail.com>
2015-01-25 13:19:48 -0500

    count in the right direction
    if we want the largest prime factor, no sense testing factors in the
    order of bottom up and use the language's built-in primality testing

--- euler3.rb
+++ euler3.rb
@@ -1,18 +1,22 @@
 # The prime factors of 13195 are 5, 7, 13 and 29.
+require 'prime'
 
 # What is the largest prime factor of the number 600851475143 ?
 
-def factor(f)
-  c=[]
-  (2..(f-1)).each do |a|
+def largest_prime_factor(f)
+  fs=[]
+  ((f/2).downto 2).each do |a|
     if f % a == 0
-      c<<a
+      fs<<a
+      return a if a.prime?
     end
   end
-  c.select {|b| factor(b).length == 0}
+  return fs
 end
 
 #puts factor 35
-p (factor 13195).last
-p (factor 600851475143).last
+p largest_prime_factor 13195
+puts "should be 29"
+p largest_prime_factor 600851475143
+puts "should be ???"
 #puts "Answer:"
